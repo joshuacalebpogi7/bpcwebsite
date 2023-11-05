873247dad0875e342228a50c0236a1458527fdd9
CULPRIT10 <gabrielberba2@gmail.com>
2023-11-05 22:58:50 +0800

    kgjlgkhjhj

--- resources/views/livewire/multi-step-form.blade.php
+++ resources/views/livewire/multi-step-form.blade.php
@@ -7,7 +7,7 @@
         @if ($currentStep == 1)
             <div class="step-one">
                 <div class="card">
-                    <div class="card-header bg-secondary text-white">STEP 1/4 - Personal Details</div>
+                    <div class="card-header text-white" style="background: #00a56f">STEP 1/4 - Personal Details</div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
@@ -85,7 +85,7 @@
         @if ($currentStep == 2)
             <div class="step-two">
                 <div class="card">
-                    <div class="card-header bg-secondary text-white">STEP 2/4 - Career Details</div>
+                    <div class="card-header text-white" style="background: #00a56f">STEP 2/4 - Career Details</div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-4">
@@ -250,7 +250,7 @@
         @if ($currentStep == 3)
             <div class="step-three">
                 <div class="card">
-                    <div class="card-header bg-secondary text-white">STEP 3/4 - Change Your Password</div>
+                    <div class="card-header text-white" style="background: #00a56f">STEP 3/4 - Change Your Password</div>
                     <div class="card-body">
                         <div class="row">
                             <div class="form-group">
@@ -288,7 +288,7 @@
         @if ($currentStep == 4)
             <div class="step-four">
                 <div class="card">
-                    <div class="card-header bg-secondary text-white">STEP 4/4 - Add Avatar</div>
+                    <div class="card-header text-white" style="background: #00a56f">STEP 4/4 - Add Avatar</div>
                     <div class="card-body">
                         <div class="image-area mt-4">
                             @if ($avatar)
@@ -336,4 +336,20 @@
 
         </div>
     </form>
+</div>
+
+<div class="svg1">
+    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#C4E8C2"
+            d="M29.6,-49.1C39.2,-45.8,48.4,-39.5,53.5,-30.8C58.5,-22.2,59.5,-11.1,59.2,-0.1C59,10.8,57.5,21.6,55.7,35.9C53.9,50.2,51.7,68,42.4,77.4C33,86.9,16.5,87.9,3.9,81.1C-8.7,74.3,-17.4,59.7,-26.4,50.1C-35.4,40.5,-44.7,35.8,-56.8,28.3C-69,20.8,-83.9,10.4,-87.5,-2C-91,-14.5,-83.1,-29,-73.3,-40.5C-63.5,-52.1,-51.8,-60.8,-39.3,-62.3C-26.7,-63.9,-13.4,-58.3,-1.7,-55.5C10,-52.6,20.1,-52.3,29.6,-49.1Z"
+            transform="translate(100 100)" />
+    </svg>
+</div>
+
+<div class="svg2">
+    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#67D199"
+            d="M48.8,-47.3C63.7,-33.9,76.5,-16.9,76.8,0.3C77.1,17.5,64.7,34.9,49.8,45.6C34.9,56.2,17.5,60,-2.5,62.5C-22.4,65,-44.8,66.1,-58.4,55.5C-72,44.8,-76.8,22.4,-73.9,2.9C-70.9,-16.5,-60.2,-33,-46.6,-46.5C-33,-60,-16.5,-70.5,0.2,-70.7C16.9,-70.9,33.9,-60.8,48.8,-47.3Z"
+            transform="translate(100 100)" />
+    </svg>
 </div>
